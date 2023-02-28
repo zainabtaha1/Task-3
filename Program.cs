4e669381af307def5512b645e1ce75c331e2e4d9
zainabtaha1 <121588138+zainabtaha1@users.noreply.github.com>
2023-02-28 03:52:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int f = int.Parse(Console.ReadLine());
+// القيمة اللي هيدخلها المستخدم هتتحول الي int وتتخزن ف f
+int C;
+// عرف متغير اسمهC نوعه int 
+C = 5 / 9*(f - 32);
+Console.WriteLine("The temperature in Fahrenheit " + f);
+// المستخدم هيدخل درجة الحراره بالفهرنهايت وهتتخزن ف f
+Console.WriteLine("The temperature in Celsius is :" + C);
+// ويحط قيمة C من خلال المعادلهThe temperature in Celsius isالمترجم هيطبع  
